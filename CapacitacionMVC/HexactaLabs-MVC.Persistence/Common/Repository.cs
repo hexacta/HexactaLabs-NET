7879afe641a914ba27c1ad11aa3f498b099cf3d1
reynaldocartana <rcartana@hexacta.com>
2015-09-15 16:02:33 -0300

    Se agregan archivos que no estaba siendo trackeados

---
+++ CapacitacionMVC/HexactaLabs-MVC.Persistence/Common/Repository.cs
@@ -0,0 +1,79 @@
+﻿using HexactaLabs_MVC.Business.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexactaLabs_MVC.Persistence.Common
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly MoviesContext Context;
+
+        public Repository(MoviesContext context)
+        {
+            this.Context = context;
+        }
+
+        protected IDbSet<T> Entities
+        {
+            get
+            {
+                return this.Context.Set<T>();
+            }
+        }
+
+        public virtual void Save(T entity)
+        {
+            this.Entities.Add(entity);
+
+            this.Context.SaveChanges();
+        }
+
+        public virtual IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return this.Entities.Where(predicate).ToList();
+        }
+
+        public virtual IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            return this.Entities.Where(predicate);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return this.Entities.ToList();
+        }
+
+        public virtual T First(Expression<Func<T, bool>> predicate)
+        {
+            return this.Entities.Where(predicate).FirstOrDefault();
+        }
+
+        public virtual T Find(params object[] keyValues)
+        {
+            return this.Entities.Find(keyValues);
+        }
+
+        public virtual void Update(T entity)
+        {
+            this.Context.Entry(entity).State = EntityState.Modified;
+            this.Context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.Context.Entry(entity).State = EntityState.Deleted;
+            this.Context.SaveChanges();
+        }
+
+        public virtual void Detach(T entity)
+        {
+            this.Context.Entry(entity).State = EntityState.Detached;
+        }
+    }
+
+}
---
+++ CapacitacionMVC/HexactaLabs-MVC.Services/Movies/MovieService.cs
@@ -0,0 +1,39 @@
+﻿using HexactaLabs_MVC.Business.Common;
+using HexactaLabs_MVC.Business.Movies;
+using HexactaLabs_MVC.Dtos.Movies;
+using HexactaLabs_MVC.IServices.Movies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexactaLabs_MVC.Services.Movies
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IRepository<Movie> movieRepository;
+
+        public MovieService(IRepository<Movie> movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+
+        public MovieDto GetMovie(int id)
+        {
+            Movie movie = movieRepository.First(x => x.Id == id);
+
+            //TODO: REFACTOR - De que otra manera se podria hacer?
+            return new MovieDto()
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                ReleaseDate = movie.ReleaseDate,
+                Plot = movie.Plot,
+                CoverLink = movie.CoverLink,
+                Runtime = movie.Runtime,
+                GenresIds = movie.Genres.Select(x => x.Id).ToList()
+            };
+        }
+    }
+}
