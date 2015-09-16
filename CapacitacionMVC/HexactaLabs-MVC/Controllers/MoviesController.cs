af631b2abc36490534869102c731ef3b46255045
reynaldocartana <rcartana@hexacta.com>
2015-09-16 15:32:20 -0300

    Se agrega a la solución inicial el controller de movies para orientar más al estudiante

---
+++ CapacitacionMVC/HexactaLabs-MVC/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using HexactaLabs_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HexactaLabs_MVC.Views.Movies
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }               
+    }
+}
